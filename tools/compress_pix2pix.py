1dd85b1977ddbdea66d297fced5507c81bbf5c41
wujianP <1051233700@qq.com>
2023-08-21 17:36:12 +0800

    add prompt to prompt

--- tools/compress_pix2pix.py
+++ tools/compress_pix2pix.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import os
 import argparse
+import shutil
 
 
 if __name__ == '__main__':
@@ -12,53 +13,31 @@ if __name__ == '__main__':
 
     target_resolution = (args.target_resolution, args.target_resolution)
 
-    # 构建target目录结构
+    # 遍历所有子目录
+    total = len(os.listdir(args.source_dir))
+    idx = 0
     for dirname in sorted(os.listdir(args.source_dir)):
-        os.makedirs(os.path.join(args.target_dir, dirname), exist_ok=True)
-
-    # for dirname in sorted(os.listdir(args.source_dir)):
-
-    # 遍历源数据集目录
-    # for root, dirs, files in os.walk(args.source_dir):
-    #
-    #     for file in files:
-    #         source_file_path = os.path.join(root, file)
-    #         target_file_path = os.path.join(target_dir, file)
-    #
-    #         # 如果是图片文件（以.jpg结尾），则压缩到目标分辨率
-    #         if file.lower().endswith('.jpg'):
-    #             with Image.open(source_file_path) as img:
-    #                 img = img.resize(target_resolution, Image.ANTIALIAS)
-    #                 img.save(target_file_path, "JPEG")
-    #
-    #         # 否则，直接复制文件
-    #         else:
-    #             shutil.copy2(source_file_path, target_file_path)
-    #
-    # print("压缩和复制完成")
-    #
-    # # 复制源数据集的目录结构到目标数据集
-    # shutil.copytree(args.source_dir, args.target_dir)
-    #
-    # # 遍历目标数据集的文件夹和文件
-    # for root, dirs, files in os.walk(args.target_dir):
-    #     for filename in files:
-    #         # 文件的完整路径
-    #         file_path = os.path.join(root, filename)
-    #
-    #         # 如果文件是图像文件（以.jpg结尾），则调整分辨率并保存
-    #         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-    #             try:
-    #                 with Image.open(file_path) as img:
-    #                     img = img.resize(target_resolution, Image.ANTIALIAS)
-    #                     img.save(file_path)
-    #                 print(f"Resized {file_path}")
-    #             except Exception as e:
-    #                 print(f"Error processing {file_path}: {e}")
-    #         else:
-    #             # 如果文件不是图像文件，直接复制到目标目录
-    #             relative_path = os.path.relpath(file_path, target_dataset_path)
-    #             target_file_path = os.path.join(target_dataset_path, relative_path)
-    #             os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
-    #             shutil.copy(file_path, target_file_path)
-    #             print(f"Copied {file_path} to {target_file_path}")
+        target_dir_path = os.path.join(args.target_dir, dirname)
+        current_dir_path = os.path.join(args.source_dir, dirname)
+        if dirname.endswith('.json'):
+            shutil.copy(current_dir_path, target_dir_path)
+            break
+        # 创建文件夹
+        os.makedirs(target_dir_path, exist_ok=True)
+        # 拷贝文件
+        for filename in sorted(os.listdir(current_dir_path)):
+            current_file_path = os.path.join(current_dir_path, filename)
+            target_file_path = os.path.join(target_dir_path, filename)
+            # 压缩图片并拷贝
+            if filename.endswith('.jpg'):
+                with Image.open(current_file_path) as img:
+                    img = img.resize(target_resolution, Image.ANTIALIAS)
+                    img.save(target_file_path, "JPEG")
+            # 直接复制其他文件
+            else:
+                shutil.copy(current_file_path, target_file_path)
+        # 显示进度
+        idx += 1
+        if idx % 100 == 0:
+            print(f'{idx} / {total} ({idx/total*100}%)')
+    print('Dataset compress DONE!')
