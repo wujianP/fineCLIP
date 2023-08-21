67cf1e6dd176fb8f9add25c3fb044a4ec223e450
wujianP <1051233700@qq.com>
2023-08-21 10:25:45 +0800

    add prompt to prompt

--- neg_clip/script.sh
+++ neg_clip/script.sh
@@ -1,14 +1,14 @@
-conda activate /discobox/wjpeng/env/openVCLIP
+conda activate /discobox/wjpeng/env/clip
 cd /discobox/wjpeng/code/202306/fineCLIP/neg_clip/src/training_pix2pix
 
-python main.py \
-    --gpus 4 \
-    --logs="/discobox/wjpeng/ckp/negCLIP/ablate/lr" \
+python -m torch.distributed.launch --nproc_per_node 2 --master_port 5 main.py \
+    --gpus 4,5 \
+    --logs="/discobox/wjpeng/ckp/negCLIP/test" \
     --name="lr5e-5_10ep_warm100_bs256_vitb32" \
     --train-data="/discobox/wjpeng/dataset/pix2pix/train_neg_clip.pth" \
     --val-data="/discobox/wjpeng/dataset/pix2pix/val_neg_clip.pth"  \
     --data-root="/discobox/wjpeng/dataset/pix2pix/clip-filtered-dataset" \
-    --batch-size=256 \
+    --batch-size=128 \
     --dataset-type="torch" \
     --epochs=10 \
     --lr=5e-5 \
