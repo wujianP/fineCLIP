71ccf5cb73864a3d3ec2d9dd87d78beae43f86f7
wujianP <1051233700@qq.com>
2023-09-13 20:21:34 +0800

    test clip on the generated dataset

--- findDifference/main.py
+++ findDifference/main.py
@@ -16,9 +16,9 @@ def main():
         ann=args.ann
     )
 
-    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
-    tokenizer = open_clip.get_tokenizer('ViT-B-32')
-    model = model.cuda()
+    # model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+    # tokenizer = open_clip.get_tokenizer('ViT-B-32')
+    # model = model.cuda()
 
     for i in range(len(dataset)):
         image, ipt_image, caption, ipt_caption = dataset[i]
