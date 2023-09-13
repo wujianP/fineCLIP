558d2767b5839055230501466c666c82ae6b30b8
wujianP <1051233700@qq.com>
2023-09-13 19:51:43 +0800

    test clip on the generated dataset

--- findDifference/main.py
+++ findDifference/main.py
@@ -1,20 +1,42 @@
 import argparse
+import torch
+import open_clip
 
 
 from dataset import MyDataset
+from PIL import Image
 # from torch.utils.data import DataLoader
 
 
 def main():
+
     dataset = MyDataset(
         data_root=args.data_root,
         ann=args.ann
     )
 
+    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+    tokenizer = open_clip.get_tokenizer('ViT-B-32')
+    model = model.cuda()
+
     for i in range(len(dataset)):
         image, ipt_image, caption, ipt_caption = dataset[i]
-        from IPython import embed
-        embed()
+        image = preprocess(image.resize((512, 512)))
+        ipt_image = preprocess(ipt_image.resize((512, 512)))
+
+        images = torch.stack([image, ipt_image], dim=0).cuda()
+        text = tokenizer([caption]).cuda()
+
+        with torch.no_grad():
+            image_features = model.encode_image(images)
+            text_features = model.encode_text(text)
+
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+            image_probs = (100.0 * text_features @ image_features.T).softmax(dim=-1)
+
+            print(image_probs)
 
 
 if __name__ == '__main__':
