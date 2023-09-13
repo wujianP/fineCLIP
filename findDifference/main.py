73e48813bb19b695b78a337bd5ea2dc24d5d3dbd
wujianP <1051233700@qq.com>
2023-09-13 20:19:25 +0800

    test clip on the generated dataset

--- findDifference/main.py
+++ findDifference/main.py
@@ -1,7 +1,8 @@
 import argparse
 import torch
 import open_clip
-
+import wandb
+wandb.login()
 
 from dataset import MyDataset
 from PIL import Image
@@ -21,27 +22,25 @@ def main():
 
     for i in range(len(dataset)):
         image, ipt_image, caption, ipt_caption = dataset[i]
-        image = preprocess(image.resize((512, 512)))
-        ipt_image = preprocess(ipt_image.resize((512, 512)))
+        # image = preprocess(image.resize((512, 512)))
+        # ipt_image = preprocess(ipt_image.resize((512, 512)))
+        #
+        # images = torch.stack([image, ipt_image], dim=0).cuda()
+        # text = tokenizer([caption]).cuda()
 
-        images = torch.stack([image, ipt_image], dim=0).cuda()
-        text = tokenizer([caption]).cuda()
+        # with torch.no_grad():
+        #     image_features = model.encode_image(images)
+        #     text_features = model.encode_text(text)
+        #
+        #     image_features /= image_features.norm(dim=-1, keepdim=True)
+        #     text_features /= text_features.norm(dim=-1, keepdim=True)
+        #
+        #     img_sim = (image_features @ image_features.T) * 100
+        #     # print(img_sim)
+        #
+        #     image_probs = (100.0 * text_features @ image_features.T).softmax(dim=-1)
 
-        with torch.no_grad():
-            image_features = model.encode_image(images)
-            from IPython import embed
-            embed()
-            img_sim = (image_features[0] * image_features[1].T) * 100
-            print(img_sim)
-
-            text_features = model.encode_text(text)
-
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-            text_features /= text_features.norm(dim=-1, keepdim=True)
-
-            image_probs = (100.0 * text_features @ image_features.T).softmax(dim=-1)
-
-            print(image_probs)
+        run.log({'Find Difference': [wandb.Image(image, caption=caption), wandb.Image(ipt_image, caption=ipt_image)]})
 
 
 if __name__ == '__main__':
@@ -49,4 +48,5 @@ if __name__ == '__main__':
     parser.add_argument('--data_root', type=str, default='./data/remove-stable-diffusion-filtered')
     parser.add_argument('--ann', type=str, default='./data/remove-stable-diffusion-refine_caption_outputs.json')
     args = parser.parse_args()
+    run = wandb.init('find difference')
     main()
