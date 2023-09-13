58978746f22145357c6d92d9a603ddd6066623e1
wujianP <1051233700@qq.com>
2023-09-13 20:36:22 +0800

    test clip on the generated dataset

--- findDifference/main.py
+++ findDifference/main.py
@@ -16,30 +16,31 @@ def main():
         ann=args.ann
     )
 
-    # model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
-    # tokenizer = open_clip.get_tokenizer('ViT-B-32')
-    # model = model.cuda()
+    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+    tokenizer = open_clip.get_tokenizer('ViT-B-32')
+    model = model.cuda()
 
     for i in range(len(dataset)):
         image, ipt_image, caption, ipt_caption = dataset[i]
-        # image = preprocess(image.resize((512, 512)))
-        # ipt_image = preprocess(ipt_image.resize((512, 512)))
-        #
-        # images = torch.stack([image, ipt_image], dim=0).cuda()
-        # text = tokenizer([caption]).cuda()
+        image = preprocess(image.resize((512, 512)))
+        ipt_image = preprocess(ipt_image.resize((512, 512)))
 
-        # with torch.no_grad():
-        #     image_features = model.encode_image(images)
-        #     text_features = model.encode_text(text)
-        #
-        #     image_features /= image_features.norm(dim=-1, keepdim=True)
-        #     text_features /= text_features.norm(dim=-1, keepdim=True)
-        #
-        #     img_sim = (image_features @ image_features.T) * 100
-        #     # print(img_sim)
-        #
-        #     image_probs = (100.0 * text_features @ image_features.T).softmax(dim=-1)
-        run.log({'Find Difference': [wandb.Image(image, caption=caption), wandb.Image(ipt_image, caption=ipt_caption)]})
+        images = torch.stack([image, ipt_image], dim=0).cuda()
+        text = tokenizer([caption]).cuda()
+
+        with torch.no_grad():
+            image_features = model.encode_image(images)
+            text_features = model.encode_text(text)
+
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+            img_sim = (image_features @ image_features.T) * 100
+            # print(img_sim)
+
+            image_probs = (100.0 * text_features @ image_features.T).softmax(dim=-1)
+
+            print(image_probs.item())
 
 
 if __name__ == '__main__':
