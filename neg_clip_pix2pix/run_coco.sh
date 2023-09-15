5f1c6bfab55b39bda752fd0d30ea495519c5b690
wujianP <1051233700@qq.com>
2023-09-15 21:25:35 +0800

    negCLIP

--- neg_clip_pix2pix/run_coco.sh
+++ neg_clip_pix2pix/run_coco.sh
@@ -2,14 +2,14 @@ conda activate /discobox/wjpeng/env/clip
 cd /discobox/wjpeng/code/202306/fineCLIP/neg_clip/src/training
 git pull
 
-CUDA_VISIBLE_DEVICES=0 python -m training.main \
+CUDA_VISIBLE_DEVICES=0 python -m main \
     --train-data="data/train_neg_clip.tsv" \
     --val-data="data/valid_neg_clip.tsv"  \
+    --logs="/DDN_ROOT/wjpeng/ckp/negCLIP/official" \
     --batch-size=256 \
     --epochs=5 \
     --name="negclip_256_1e-6" \
     --lr=1e-6 \
-    --logs="./logs/negCLIP/" \
     --pretrained="openai" \
     --model="ViT-B-32"\
     --workers 14 \
