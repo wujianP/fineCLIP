f13984128dc1c889a178cf8d0017ab5e8ba31da3
wujianP <1051233700@qq.com>
2023-09-18 14:23:48 +0800

    explore dataset

--- benchmarks/scripts/run_eval_CLIP.sh
+++ benchmarks/scripts/run_eval_CLIP.sh
@@ -2,6 +2,6 @@ conda activate /discobox/wjpeng/env/clip
 cd /discobox/wjpeng/code/202306/fineCLIP/benchmarks/EqBen/example
 git pull
 export CUDA_VISIBLE_DEVICES=1
-python eqben_eval_CLIP.py --eval_data winoground --batch_size 256 \
---resume /DDN_ROOT/wjpeng/ckp/negCLIP/mine/negclip_128_1e-6_fastchat/checkpoints/epoch_5.pt \
---load_file_basename fastchat_vitb_32
+python eqben_eval_CLIP.py --eval_data eqben --batch_size 256 \
+--resume /DDN_ROOT/wjpeng/ckp/negCLIP/official/negclip_128_1e-6/checkpoints/epoch_5.pt \
+--load_file_basename negCLIP_vitb_32
